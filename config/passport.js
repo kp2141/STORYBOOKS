acee3d969e2a63ec18e179c7ac0f79ae62010790
kp2141 <kp2141krp@gmail.com>
2021-05-17 14:20:09 -0400

    updated google callback and changed app name

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ module.exports = function(passport){
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL:'https://guarded-beyond-89346.herokuapp.com/auth/google/callback'
+        callbackURL:'https://kevin-storybooks.herokuapp.com/auth/google/callback'
     },
     async(accessToken,refreshToken,profile,done) => {
         const newUser = {
